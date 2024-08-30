39bae4085e1deb1132ef89d1966cf1c8b50b8bec
SJGit15 <153937670+SJGit15@users.noreply.github.com>
2024-08-30 19:23:10 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function showDiv() {
+    document.getElementById('appear').style.display='grid';
+}
+function changeColor() {
+    document.getElementById("color").style.cssText=`background-color: hsl(217, 19%, 35%)`;
+}
+
